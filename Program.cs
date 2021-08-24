b6899cdde1a7ae5a3baa45cd4f5f5b3ce0b06e45
vasantha <vasantha.glh@gmail.com>
2021-08-24 20:07:31 +0530

    Employee class defined

--- Program.cs
+++ Program.cs
@@ -2,11 +2,23 @@
 
 namespace SampleApp
 {
+    class Employee
+    {
+        public int Id  {get; set;}
+        public string Name {get; set;}
+        public double Salary {get; set;}
+        public override string ToString()
+        {
+            return $"{Id} - {Name} - {Salary}";
+        }
+    }
     class Program
     {
-        static void Main(string[] args)
+            static void Main(string[] args)
         {
             Console.WriteLine("Hello World Updated!");
+            Employee employee = new Employee {Id = 101,Name = "Hema", Salary = 95000.00};
+            Console.WriteLine(employee);
         }
     }
 }
